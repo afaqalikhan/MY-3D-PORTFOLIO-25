d7d34cf9b1d21a463f1a5be0d13df21061923467
afaqalikhan <afaqalikhan02@gmail.com>
2025-10-26 19:55:52 +0500

    About Section

--- 3d/src/App.jsx
+++ 3d/src/App.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import NavBar from "./sections/NavBar";
 import Hero from "./sections/Hero";
+import About from "./sections/About";
 
 const App = () => {
   return (
-    // git push -f
     <div className="container mx-auto max-w-7xl">
       <NavBar />
       <Hero />
+      <About />
     </div>
   );
 };
---
+++ 3d/src/sections/About.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className='c-space section-spacing'>
+      <h2 className='text-heading'>About Me</h2>
+      {/* <div className='grid grid-cols-1'>
+
+      </div> */}
+
+      {/* Grid 1 */}
+      <div className='flex items-end grid-default-color grid-1'></div>
+
+    </section>
+  )
+}
+
+export default About
